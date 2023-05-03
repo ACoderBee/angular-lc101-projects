4c591137a052c9456b9bb0dcdbdda926074cbd18
Andrea <93172228+Arionly@users.noreply.github.com>
2023-05-03 17:01:57 -0500

    Lost in angular ell

--- lesson1/exercises/src/app/movie-list/movie-list.component.ts
+++ lesson1/exercises/src/app/movie-list/movie-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['The Manchurian Candidate', 'Oceans 8'];
+   movies = ['The Manchurian Candidate', 'Oceans 8',"All About Mary", "When Sally Met Harry"];
 
    constructor() { }
 
